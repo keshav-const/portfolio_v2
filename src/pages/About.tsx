4cb916c6819d784cf2020e804002b84ee81b26a0
keshav-const <keshavladdha005@gmail.com>
2025-10-14 00:53:23 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -61,7 +61,7 @@ export default function About() {
 
             <div className="pt-6 flex gap-4">
               <a
-              href="https://drive.google.com/file/d/1fZwpd6UIVzUndB6IWTRoZOagOkjREkTx/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1WtHyiQFY9ci61MzmUZ2vTQfvZs13kjbt/view?usp=drive_link"
               download="Keshav_Resume.pdf" 
               className="flex items-center gap-2 px-8 py-4 bg-white text-black rounded-lg font-medium hover:bg-gray-200 transition-colors"
               >
